c0cd78dd5b209f7c30a982617c28b7f87a590e81
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-05-17 17:21:43 +0300

    Add playRound()

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function computerPlay() {
       throw new Error('Error in Math.random()');
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerLc = playerSelection.toLowerCase();
+  let computerLc = computerSelection.toLowerCase();
+  let winMessage = `You win! ${playerLc[0].toUpperCase() + playerLc.slice(1)} beats ${computerLc[0].toUpperCase() + computerLc.slice(1)}`;
+  let loseMessage = `You lose! ${computerLc[0].toUpperCase() + computerLc.slice(1)} beats ${playerLc[0].toUpperCase() + playerLc.slice(1)}`;
+  let drawMessage = `It's a draw! ${playerLc[0].toUpperCase() + playerLc.slice(1)} and ${computerLc[0].toUpperCase() + computerLc.slice(1)} are equal!`;
+
+  if (playerLc === computerLc) {
+    return drawMessage;
+  } 
+  
+  if (
+    (playerLc === 'rock' && computerLc === 'scissors')
+    || (playerLc === 'paper' && computerLc === 'rock')
+    || (playerLc === 'scissors' && computerLc === 'paper')
+  ) {
+    return winMessage;
+  } 
+
+  return loseMessage;
+}
